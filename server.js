f5186ee8cde7c1ecbe67cc8ecdc625aba3120d7e
Glitch (arrow1802-boilerplate-project-headerparser) <none>
2019-12-03 11:07:28 +0000

    Done1

--- server.js
+++ server.js
@@ -2,41 +2,44 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
 });
 // {"ipaddress":"89.187.145.50",
 // "language":"en-US,en;q=0.5",
 // "software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0"}
-app.get("/api/whoami", function (req, res) {
-  console.log(req.headers)
+app.get("/api/whoami", function(req, res) {
+  // console.log(req.headers);
+
+  let ip = req.headers["x-forwarded-for"].split(",")[0];
+  let lang = req.headers["accept-language"];
+  let userAgent = req.headers["user-agent"];
+  // console.log(ip)
   res.json({
-    "ipaddress":"",
-    "language":"",
-    "software":""
+    ipaddress: ip,
+    language: lang,
+    software: userAgent
   });
 });
 
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
